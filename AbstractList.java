84cb2111a3c1dfef7bf7450fa5b3c8fd58d4fcde
Estiby <estiwnl@gmail.com>
2026-02-24 20:42:02 -0600

    Se implementó la clase abstracta AbstractList con manejo base del tamaño y verificación de lista vacía.

---
+++ AbstractList.java
@@ -0,0 +1,13 @@
+public abstract class AbstractList<E> implements ListADT<E> {
+    protected int count;
+
+    @Override
+    public boolean isEmpty() {
+        return count == 0;
+    }
+
+    @Override
+    public int size() {
+        return count;
+    }
+}
