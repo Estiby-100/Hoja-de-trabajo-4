cda4726e9b4917445c2feb2c9b029bdb22793015
Estiby <estiwnl@gmail.com>
2026-02-24 20:40:48 -0600

    Se implementó la clase abstracta AbstractStack que define comportamiento común de la pila y manejo del contador.

---
+++ AbstractStack.java
@@ -0,0 +1,13 @@
+public abstract class AbstractStack<E> implements Stack<E> {
+    protected int count;
+
+    @Override
+    public boolean isEmpty() {
+        return count == 0;
+    }
+
+    @Override
+    public int size() {
+        return count;
+    }
+}
